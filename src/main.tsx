170cf639d63e528c8199775bc103f89b4c8ecc7d
Tahir Iqbal <shahtahir786@gmail.com>
2025-02-12 19:21:05 +0530

    add: tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 import "./index.css";
+import MyLoader from "./Loading.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App />
+            <Suspense fallback={<MyLoader />}>
+                <App />
+            </Suspense>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     </StrictMode>
