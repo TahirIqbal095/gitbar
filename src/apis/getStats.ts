b08b1be7b9daef1ffca629848639355d57cd3bd3
Tahir Iqbal <shahtahir786@gmail.com>
2025-02-13 09:34:19 +0530

    remove useSuspenseQuery

--- src/apis/getStats.ts
+++ src/apis/getStats.ts
@@ -1,23 +1,16 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
 import { Octokit } from "octokit";
 
 const octokit = new Octokit({
     auth: import.meta.env.VITE_GITHUB_TOKEN,
 });
 
-export default function getStats(username: string) {
-    const data = useSuspenseQuery({
-        queryKey: ["stats"],
-        queryFn: async () => {
-            await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a slow network request
-            return await octokit.request(`GET /users/${username}`, {
-                username: import.meta.env.VITE_USERNAME,
-                headers: {
-                    accept: "application/vnd.github+json",
-                    "X-GitHub-Api-Version": "2022-11-28",
-                },
-            });
+export default async function getStats(username: string) {
+    const response = await octokit.request(`GET /users/${username}`, {
+        username: import.meta.env.VITE_USERNAME,
+        headers: {
+            accept: "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
         },
     });
-    return data;
+    return response.data;
 }
