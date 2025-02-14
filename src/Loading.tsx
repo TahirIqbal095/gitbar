726a51c22087a6375199b2fd8d933b81ab4627e4
Tahir Iqbal <shahtahir786@gmail.com>
2025-02-14 11:39:44 +0530

    loading skelton fix

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -3,17 +3,18 @@ import ContentLoader from "react-content-loader";
 const MyLoader = () => (
     <ContentLoader
         speed={2}
-        width={400}
-        height={160}
-        viewBox="0 0 400 160"
+        width={"100%"}
+        height={"100%"}
+        viewBox="0 0 400 320"
         backgroundColor="#f3f3f3"
         foregroundColor="#ecebeb"
-        className="w-full h-[50vh]"
+        className="w-full"
     >
-        <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
-        <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
-        <rect x="0" y="56" rx="3" ry="3" width="410" height="139" />
-        <circle cx="20" cy="20" r="20" />
+        <rect x="98" y="8" rx="3" ry="3" width="88" height="6" />
+        <rect x="98" y="26" rx="3" ry="3" width="52" height="6" />
+        <rect x="98" y="40" rx="3" ry="3" width="30" height="6" />
+        <rect x="0" y="96" rx="10" ry="10" width="100%" height="100%" />
+        <circle cx="40" cy="40" r="40" />
     </ContentLoader>
 );
 
