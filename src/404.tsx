26580d2efe6b249d8899fc75ca701ba339baafde
Tahir Iqbal <shahtahir786@gmail.com>
2025-02-15 16:34:38 +0530

    404 error

---
+++ src/404.tsx
@@ -0,0 +1,7 @@
+export function NotFound() {
+    return (
+        <div className="flex justify-center items-center mt-6">
+            <h1 className="text-4xl font-bold text-red-500">404 | Not Found</h1>
+        </div>
+    );
+}
